042690eef6de3e66ce8623a6643b7c60d1e34a99
unknown <Sagar>
2015-10-20 17:36:11 -0700

    Added Onclick functionality to 'Sign up' button

--- app/src/main/java/com/example/saiprashanth/mainproject/MainActivity.java
+++ app/src/main/java/com/example/saiprashanth/mainproject/MainActivity.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUp=(Button)findViewById(R.id.signUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+            Intent intentSignUp=new Intent(getApplicationContext(),signupPage.class);
+                startActivity(intentSignUp);
+            }
+        });
     }
 
     @Override
